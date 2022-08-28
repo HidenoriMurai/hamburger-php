3c25da163bc139d18ed876bb5547ea69bcbd2b77
Hidenori Murai <hidenoweb822@gmail.com>
2022-08-28 11:09:04 +0900

    検索結果表示追加

--- 404.php
+++ 404.php
@@ -1,6 +1,9 @@
 <?php get_header(); ?>
     <div id="content" class="narrowcolumn">
-        <h2 class="center">Error 404 - Not Found</h2>
+        <h2>お探しのページが見つかりませんでした（404 Not Found）</h2>
+        <h3>指定された以下のページは存在しないか、または移動した可能性があります。</h3>
+        <a href="<?php echo home_url(); ?>">Hamburger</a>へ戻る
     </div>
+</section>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- search.php
+++ search.php
@@ -3,76 +3,40 @@
 
                 <div class="p-archive-search__top">
                     <div class="p-archive-search__top-bg">
-                        <p class="p-archive-search__top-sub-title">Search:</p><p class="p-archive-search__top-sub-title-1">チーズバーガー</p>
+                        <p class="p-archive-search__top-sub-title">Search:</p><p class="p-archive-search__top-sub-title-1"><?php the_title(); ?></p>
                         <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/archive-top-sp.jpg" alt="top-images" class="p-archive-search__top-images">
                     </div>
                 </div>
 
-                <div class="c-text-box">
-                    <h2 class="c-text-box__ttl">小見出しが入ります</h2>
-                    <p class="c-text-box__text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-                </biv>
+
+                <p class="p-archive-search-snumber">『<?php the_title(); ?>』の検索結果:<?php echo $wp_query->found_posts; ?>件</p>
+
+                <?php
+                    if ( have_posts()) : 
+                    while ( have_posts()) :
+                        the_post();
+                ?>
+
 
                 <div class="c-archive-box">
-                    <img src="/images/ archive-pc.png" alt="archive-box-image" class="c-archive-box__image">
+                    <div class="c-archive-box__image" ><?php the_post_thumbnail(); ?></div>
                     <div class="c-archive-box__inner">
-                        <p class="c-archive-box__ttl">チーズバーガー</p>
+                        <p class="c-archive-box__ttl"><?php the_title(); ?></p>
                         <h3 class="c-archive-box__sub-ttl">小見出しが入ります</h3>
                         <p class="c-archive-box__text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
                         <div class="c-btn__archive-layout">
-                            <button class="c-btn__archive">詳しく見る</button>
+                            <button class="c-btn__archive">
+                                <a href="<?php the_permalink($id); ?>">詳しく見る</a>
+                            </button>
                         </div>
                     </div>
                 </div>
 
-                <div class="c-archive-box">
-                    <img src="/images/ archive-pc.png" alt="archive-box-image" class="c-archive-box__image">
-                    <div class="c-archive-box__inner">
-                        <p class="c-archive-box__ttl">チーズバーガー</p>
-                        <h3 class="c-archive-box__sub-ttl">小見出しが入ります</h3>
-                        <p class="c-archive-box__text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-                        <div class="c-btn__archive-layout">
-                            <button class="c-btn__archive">詳しく見る</button>
-                        </div>
-                    </div>
-                </div>
+                <?php
+                    endwhile;
+                    endif;
+                ?>
 
-                <div class="c-archive-box">
-                    <img src="/images/ archive-pc.png" alt="archive-box-image" class="c-archive-box__image">
-                    <div class="c-archive-box__inner">
-                        <p class="c-archive-box__ttl">チーズバーガー</p>
-                        <h3 class="c-archive-box__sub-ttl">小見出しが入ります</h3>
-                        <p class="c-archive-box__text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-                        <div class="c-btn__archive-layout">
-                            <button class="c-btn__archive">詳しく見る</button>
-                        </div>
-                    </div>
-                </div>
-
-
-                <div class="c-archive-box">
-                    <img src="/images/ archive-pc.png" alt="archive-box-image" class="c-archive-box__image">
-                    <div class="c-archive-box__inner">
-                        <p class="c-archive-box__ttl">チーズバーガー</p>
-                        <h3 class="c-archive-box__sub-ttl">小見出しが入ります</h3>
-                        <p class="c-archive-box__text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-                        <div class="c-btn__archive-layout">
-                            <button class="c-btn__archive">詳しく見る</button>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="c-archive-box">
-                    <img src="/images/ archive-pc.png" alt="archive-box-image" class="c-archive-box__image">
-                    <div class="c-archive-box__inner">
-                        <p class="c-archive-box__ttl">チーズバーガー</p>
-                        <h3 class="c-archive-box__sub-ttl">小見出しが入ります</h3>
-                        <p class="c-archive-box__text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-                        <div class="c-btn__archive-layout">
-                            <button class="c-btn__archive">詳しく見る</button>
-                        </div>
-                    </div>
-                </div>
 
                 <div class="p-archive__pagination">
                     <button class="p-archive__pagination-previous">
